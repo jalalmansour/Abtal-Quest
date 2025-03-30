c309c202f71afbedd61ec10e822dce37cc506b97
Jalal Mansour <42254243+jalalmansour@users.noreply.github.com>
2025-03-30 16:55:13 +0000

    Create PartnersSection.tsx

---
+++ src/components/PartnersSection.tsx
@@ -0,0 +1,13 @@
+// src/components/about/PartnersSection.tsx
+const PartnersSection = () => {
+  return (
+    <section className="section-padding">
+      <div className="container-custom">
+        <h2>Partners Section</h2>
+        {/* Add your partners section content here */}
+      </div>
+    </section>
+  );
+};
+
+export default PartnersSection;
