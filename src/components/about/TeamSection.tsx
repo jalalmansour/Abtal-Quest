b606bcc19efd80ad9b303d4cabe34dc082654951
Jalal Mansour <42254243+jalalmansour@users.noreply.github.com>
2025-03-30 16:53:55 +0000

    Create TeamSection.tsx

---
+++ src/components/about/TeamSection.tsx
@@ -0,0 +1,13 @@
+// src/components/about/TeamSection.tsx
+const TeamSection = () => {
+  return (
+    <section className="section-padding">
+      <div className="container-custom">
+        <h2>Team Section</h2>
+        {/* Add your team section content here */}
+      </div>
+    </section>
+  );
+};
+
+export default TeamSection;
