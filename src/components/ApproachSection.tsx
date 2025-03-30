c4f6b84d9c1a7a5d78c7599752cb84c5fdc5f35c
Jalal Mansour <42254243+jalalmansour@users.noreply.github.com>
2025-03-30 16:54:41 +0000

    Create ApproachSection.tsx

---
+++ src/components/ApproachSection.tsx
@@ -0,0 +1,13 @@
+// src/components/about/
+const ApproachSection = () => {
+  return (
+    <section className="section-padding">
+      <div className="container-custom">
+        <h2>Approach Section</h2>
+        {/* Add your approach section content here */}
+      </div>
+    </section>
+  );
+};
+
+export default ApproachSection;
