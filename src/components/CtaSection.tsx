9cee66aa176eeba8b0fd4c4d69279d271d076247
Jalal Mansour <42254243+jalalmansour@users.noreply.github.com>
2025-03-30 16:55:44 +0000

    Create CtaSection.tsx

---
+++ src/components/CtaSection.tsx
@@ -0,0 +1,13 @@
+// src/components/about/CtaSection.tsx
+const CtaSection = () => {
+  return (
+    <section className="section-padding">
+      <div className="container-custom">
+        <h2>Cta Section</h2>
+        {/* Add your cta section content here */}
+      </div>
+    </section>
+  );
+};
+
+export default CtaSection;
